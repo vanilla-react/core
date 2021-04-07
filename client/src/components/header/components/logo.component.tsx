e3b57673b8f7493599430f1880de168210ff0054
Donny Roufs <iamchets@yahoo.com>
2021-04-07 22:16:17 +0200

    add breakpoints

--- client/src/components/header/components/logo.component.tsx
+++ client/src/components/header/components/logo.component.tsx
@@ -2,7 +2,7 @@ import { Box, Heading } from '@chakra-ui/react';
 import Link from 'next/link';
 
 export const Logo = () => (
-  <Box display={['none', 'none', 'none', 'block']}>
+  <Box display={['none', 'none', 'none', 'block']} cursor="pointer">
     <Link href="/">
       <Heading color="pink.400">VR</Heading>
     </Link>
