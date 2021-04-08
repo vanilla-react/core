d03075956364f5ff4fb85145570e6caa25aa8965
Donny Roufs <iamchets@yahoo.com>
2021-04-08 15:55:25 +0200

    fix comment

--- client/src/entrypoint/index.ts
+++ client/src/entrypoint/index.ts
@@ -1,15 +1,11 @@
+/* When adding new hooks or providers you need to update the interface in @/types.ts */
+
 import Axios from 'axios';
 import * as AuthModule from '../features/auth/auth.module';
 import * as SharedModule from '../features/shared/shared.module';
 
-/*
-  When adding new hooks of providers you need to update the interface in @/types.ts
-*/
-
-// export your hooks
 export const useInitialAuth = AuthModule.useInitialAuth;
 
-// bootstrap your providers
 export const config = new SharedModule.ConfigService();
 export const axios = Axios.create({
   baseURL: config.BASE_URL,
