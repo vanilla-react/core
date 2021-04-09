b39c98d293eec35cd48eccfdd79aa54e27b26608
Donny Roufs <iamchets@yahoo.com>
2021-04-09 02:41:41 +0200

    prepare test environment  :tada:

--- server/src/snippet/snippet.controller.ts
+++ server/src/snippet/snippet.controller.ts
@@ -14,22 +14,26 @@ import { UpdateSnippetDto } from './dto/update-snippet.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { ApiBearerAuth, ApiProperty, ApiTags } from '@nestjs/swagger';
 import { User } from '../auth/decorators/user.decorator';
+import { PrismaService } from '../prisma.service';
 
 @ApiTags('snippet')
 @Controller('snippet')
 @ApiBearerAuth()
 export class SnippetController {
-  constructor(private readonly snippetService: SnippetService) {}
+  constructor(
+    private readonly snippetService: SnippetService,
+    private readonly _prismaService: PrismaService,
+  ) {}
 
   @Post()
   @UseGuards(JwtAuthGuard)
-  create(@User() userId: number, @Body() createSnippetDto: CreateSnippetDto) {
-    return this.snippetService.create(userId, createSnippetDto);
+  create(@User() userId: number, @Body() createSnippetDto: any) {
+    return userId;
   }
 
   @Get()
   findAll() {
-    return this.snippetService.findAll();
+    return this._prismaService.post.findMany({});
   }
 
   @Get(':id')
